26a4e78d7033043f12806acca29fbb0600d25265
JOSE ZEPEDA <63516282+J2ZROMERO@users.noreply.github.com>
2022-12-13 14:03:05 -0600

    Add queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name like '%mon';
+select name from animals where date_of_birth between '2016-01-01' and '2019-12-31';
+select name from animals where neutered = true and escape_attempts < 3;
+select date_of_birth from animals where name in ('Agumon','Pikachu');
+select name,escape_attempts from animals where weigth_kg > 10.5;
+select * from animals where neutered = true;
+select * from animals where name not  in ('Gabumon');
+select * from animals where weigth_kg between 10.4 and 17.3;
+
