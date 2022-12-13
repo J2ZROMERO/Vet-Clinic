9652fa118c2dfac648297ed9d5a220e0a38caab3
JOSE ZEPEDA <63516282+J2ZROMERO@users.noreply.github.com>
2022-12-13 13:20:34 -0600

    Add data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals values (1,'Agumon','2020-02-03',0,false,10.23);
+insert into animals values (2,'Gabumon','2018-11-15',2,true,8);
+insert into animals values (3,'Pikachu','2021-01-07',1,true,15.04);
+insert into animals values (4,'Devimon','2017-05-12',5,true,11);
