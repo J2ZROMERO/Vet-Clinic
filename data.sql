e8fa4b1ccf4ce696788317f0ac10b622bff2e2ce
JOSE ZEPEDA <63516282+J2ZROMERO@users.noreply.github.com>
2022-12-14 16:15:05 -0600

    Add more data rows

--- data.sql
+++ data.sql
@@ -1,6 +1,9 @@
 /* Populate database with sample data. */
 
-insert into animals values (1,'Agumon','2020-02-03',0,false,10.23);
-insert into animals values (2,'Gabumon','2018-11-15',2,true,8);
-insert into animals values (3,'Pikachu','2021-01-07',1,true,15.04);
-insert into animals values (4,'Devimon','2017-05-12',5,true,11);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (5,'Charmander','2020-02-08',0,false,-11);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (6,'Plantmon','2021-11-15',2,true,-5.7);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (7,'Squirtle','1993-03-02',3,false,-12.13);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (8,'Angemon','2005-06-12',1,true,-45);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (9,'Boarmon','2005-06-07',7,true,20.4);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (10,'Blossom','1998-10-13',3,true,17);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (11,'Ditto','2022-05-14',4,true,22);
