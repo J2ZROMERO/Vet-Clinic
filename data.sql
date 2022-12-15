c4d76c77ae6c98d1b5cc5d3fd3f0cdf682b358d9
JOSE ZEPEDA <63516282+J2ZROMERO@users.noreply.github.com>
2022-12-15 09:20:07 -0600

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@
 
 insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (5,'Charmander','2020-02-08',0,false,-11);
 insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (6,'Plantmon','2021-11-15',2,true,-5.7);
-insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (7,'Squirtle','1993-03-02',3,false,-12.13);
+insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (7,'Squirtle','1993-04-02',3,false,-12.13);
 insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (8,'Angemon','2005-06-12',1,true,-45);
 insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (9,'Boarmon','2005-06-07',7,true,20.4);
 insert into animals(id,name,date_of_birth,escape_attempts,neutered,weigth_kg) values (10,'Blossom','1998-10-13',3,true,17);
